47ae557954bfa446e3a566facfc4aa532b6e7ffd
Ryuunotora <144704234+Ryuunotora@users.noreply.github.com>
2024-09-27 19:25:07 -0500

    Initial commit

---
+++ src/main/java/pe/edu/cibertec/CL1_FiegnClient/Cl1FiegnClientApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.CL1_FiegnClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl1FiegnClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl1FiegnClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/CL1_FiegnClient/Cl1FiegnClientApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.CL1_FiegnClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl1FiegnClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
